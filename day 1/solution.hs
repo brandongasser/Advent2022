568c2c537f3330a99c1fb0ab567ba147971db2f4
brandongasser <bgasser12@gmail.com>
2022-12-01 10:37:43 -0600

    day 1 solution

---
+++ day 1/solution.hs
@@ -0,0 +1,14 @@
+import Data.List
+
+main :: IO ()
+main = do input <- readFile "input.txt"
+          let nums = map (map read) $ splitOn "" $ lines input :: [[Int]]
+          print $ sum $ take 3 $ reverse $ sort $ map sum nums
+
+splitOn :: (Eq a) => a -> [a] -> [[a]]
+splitOn _ [] = [[]]
+splitOn e (x:xs) = if x == e
+                   then [] : ys
+                   else (x:head ys) : tail ys
+                        where
+                            ys = splitOn e xs

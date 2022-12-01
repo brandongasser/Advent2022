ca2a9966eadbe907a7ae647ca54c3e895fc0102c
brandongasser <bgasser12@gmail.com>
2022-12-01 11:57:51 -0600

    fixed splitOn comment

--- day 1/solution.hs
+++ day 1/solution.hs
@@ -11,7 +11,7 @@ main = do input <- readFile "input.txt"
 {-|
 Splits a list into a list of lists without including the delimeter
 
-splitOn "" ["1", "2", "", "3", "4", "5", "" "7"] => [["1", "2"], ["3", "4", "5"], ["7"]]
+splitOn "" ["", "1", "2", "", "3", "4", "5", "", "", "7", ""] => [[], ["1", "2"], ["3", "4", "5"], [], ["7"], []]
 -}
 splitOn :: (Eq a) => a -> [a] -> [[a]]
 splitOn e [] = []

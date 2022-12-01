f4a7d7f4f3de9f0be9b66fcc081d215feb7f0802
brandongasser <bgasser12@gmail.com>
2022-12-01 11:21:51 -0600

    commented solution

--- day 1/solution.hs
+++ day 1/solution.hs
@@ -8,6 +8,11 @@ main = do input <- readFile "input.txt"
           -- Part 2
           print $ sum $ take 3 $ reverse $ sort $ map sum nums
 
+{-|
+Splits a list into a list of lists without including the delimeter
+
+splitOn "" ["1", "2", "", "3", "4", "5", "" "7"] => [["1", "2"], ["3", "4", "5"], ["7"]]
+-}
 splitOn :: (Eq a) => a -> [a] -> [[a]]
 splitOn _ [] = [[]]
 splitOn e (x:xs) = if x == e

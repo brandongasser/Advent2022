8af40b221368e6633008728bd03754c5bcb932e3
brandongasser <bgasser12@gmail.com>
2022-12-01 11:56:19 -0600

    better splitOn

--- day 1/solution.hs
+++ day 1/solution.hs
@@ -14,9 +14,7 @@ Splits a list into a list of lists without including the delimeter
 splitOn "" ["1", "2", "", "3", "4", "5", "" "7"] => [["1", "2"], ["3", "4", "5"], ["7"]]
 -}
 splitOn :: (Eq a) => a -> [a] -> [[a]]
-splitOn _ [] = [[]]
-splitOn e (x:xs) = if x == e
-                   then [] : ys
-                   else (x:head ys) : tail ys
-                        where
-                            ys = splitOn e xs
+splitOn e [] = []
+splitOn e xs = if e `elem` xs
+                then takeWhile (/=e) xs : splitOn e (tail $ dropWhile (/=e) xs)
+                else [xs]

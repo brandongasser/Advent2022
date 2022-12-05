82c9de771a70482d8b6e6ed8f1f1ac8039f06bfd
brandongasser <bgasser12@gmail.com>
2022-12-05 13:46:58 -0600

    added multiple pop

--- stack.hs
+++ stack.hs
@@ -1,8 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use zipWithM" #-}
 module Stack where
 
 import Control.Monad.State (StateT, MonadState(get, put))
+import Control.Monad (replicateM, zipWithM)
 
 type Stack a = [a]
 type StackT a = StateT (Stack a) Maybe
@@ -12,7 +12,7 @@ push elem = do st <- get
                put $ elem : st
 
 pushList :: [a] -> StackT a [()]
-pushList xs = sequence $ zipWith ($) pushes xs
+pushList = zipWithM ($) pushes
     where
         pushes = push : pushes
 
@@ -22,6 +22,9 @@ pop = do xs <- get
             [] -> fail "Empty Stack"
             _  -> do put $ init xs
                      return $ last xs
+
+popN :: Int -> StackT a [a]
+popN n = replicateM n pop
 
 peek :: Stack a -> Maybe a
 peek xs

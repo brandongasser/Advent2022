fa0ef721cf25d711db9bf66cdf61e46e69a718ef
brandongasser <bgasser12@gmail.com>
2022-12-06 12:41:15 -0600

    rewrote day 1 to use parser

--- day 1/solution.hs
+++ day 1/solution.hs
@@ -1,10 +1,25 @@
-import Utils (splitOn)
 import Data.List (sort)
+import Parse (Parser, parse, newline, int)
+import Control.Applicative (Alternative((<|>), some))
+
+type Elf = [Int]
+
+elfP :: Parser Elf
+elfP = some (do x <- int
+                newline
+                return x
+              <|> int)
+
+parser :: Parser [Elf]
+parser = some (do elf <- elfP
+                  newline
+                  return elf
+                <|> elfP)
 
 main :: IO ()
 main = do input <- readFile "day 1/input.txt"
-          let nums = map (map read) $ splitOn "" $ lines input
+          let [(elves, input')] = parse parser input
           -- Part 1
-          print $ maximum $ map sum nums
+          print $ maximum $ map sum elves
           -- Part 2
-          print $ sum $ take 3 $ reverse $ sort $ map sum nums
+          print $ sum $ take 3 $ reverse $ sort $ map sum elves

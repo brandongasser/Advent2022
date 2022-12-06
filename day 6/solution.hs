4355b9b645545dc19f65c2288287c4dcaa27bfbe
brandongasser <bgasser12@gmail.com>
2022-12-06 10:56:39 -0600

    day 6 solved

---
+++ day 6/solution.hs
@@ -0,0 +1,20 @@
+import Utils (unique)
+
+main :: IO ()
+main = do input <- readFile "day 6/input.txt"
+          -- Part 1
+          print $ ans 4 input
+          -- Part 2
+          print $ ans 14 input
+
+ans :: Int -> String -> Int
+ans n input = let gs = groups n input
+              in  fst $ head $ filter (different . snd) $ zip [n..] gs
+
+groups :: Int -> [a] -> [[a]]
+groups n xs
+    | length xs < n = []
+    | otherwise     = take n xs : groups n (tail xs)
+
+different :: (Eq a) => [a] -> Bool
+different xs = unique xs == xs
